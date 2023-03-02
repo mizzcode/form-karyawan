e521aaa7f8cf8e79a21ddef542d1f38c5925b76a
Misbahudin <mizzc0d3@gmail.com>
2023-03-02 15:55:37 +0700

    add bootstrap v5

--- README.md
+++ README.md
@@ -1,35 +1,47 @@
 [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 
 # Apps Formulir Karyawan
+
 Aplikasi Formulir Karyawan Berbasis Web dapat membantu perusahaan dalam mengelola dan mengatur data karyawan secara efisien.
 
 ## Features
+
 - Menghitung Tunjangan Setiap Data Karyawan
-- Menghitung Pajak Setiap Data Karyawan 
+- Menghitung Pajak Setiap Data Karyawan
 - Menghitung Total Gaji Setiap Data Karyawan
 
 ## Tech Stack
-![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white) 	![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white) 
+
+![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white) ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
 ![PHP](https://img.shields.io/badge/php-%23777BB4.svg?style=for-the-badge&logo=php&logoColor=white)
-#### Requirement 
+
+#### Requirement
+
 - PHP >= 8.2
-- GIT : https://git-scm.com/downloads
 
 ## Installation
+
+- GIT : https://git-scm.com/downloads
+
 untuk menjalankan aplikasi form karyawan bisa clone repository agar mendapatkan source code
+
 ```sh
 git clone https://github.com/mizzcode/form-karyawan.git
 ```
-cara nya : 
+
+cara nya :
+
 - buka text editor favorit kalian, kalau saya Visual Studio Code
 - tampilan awal pada Visual Studio Code
 
 ![image](https://user-images.githubusercontent.com/101040281/221087710-29d0212c-d6f2-48a9-ad1c-be84ab04126f.png)
+
 - Klik Open Folder di menu File atau Start
 - Masuk ke Folder htdocs di XAMPP lalu klik menu Terimal > New Terminal
 
 ![image](https://user-images.githubusercontent.com/101040281/221125133-a83bf113-b965-4e20-8891-cf0894957294.png)
-- Pada Terminal ketik perintah ``` git clone https://github.com/mizzcode/form-karyawan.git ```
+
+- Pada Terminal ketik perintah `git clone https://github.com/mizzcode/form-karyawan.git`
 
 ![image](https://user-images.githubusercontent.com/101040281/221134545-44e3a19c-172f-4952-9c9e-d3d88c77f592.png)
 
@@ -37,9 +49,14 @@ cara nya :
 
 ![image](https://user-images.githubusercontent.com/101040281/221123221-87d746ab-371f-4fd3-a429-f22bde4058aa.png)
 
-- Tulis di Web Browser : ``` http://localhost/form-karyawan.php ```
+- Tulis di Web Browser : `http://localhost/form-karyawan.php`
 - Selamat! Aplikasi Formulir Karyawan Berhasil di Install dan siap digunakan.
 
 ## Snapshot atau Gambar 📸
 
-![image](https://user-images.githubusercontent.com/101040281/221137659-9328dff8-5643-4018-a838-3149f791798c.png)
+![image](https://user-images.githubusercontent.com/101040281/222379412-fedf519d-5bf7-437e-bc79-38e911d5b61f.png)
+
+## Author :
+
+- Nama : Misbahudin
+- No HP : 081286558895
---
+++ index.php
@@ -0,0 +1,283 @@
+<?php
+
+require_once __DIR__ . "/vendor/autoload.php";
+
+use RandomLib\Factory;
+use SecurityLib\Strength;
+
+$listAgama = ['Islam', 'Kristen', 'Hindu', 'Buddha', 'Katolik', 'Kong Hu Chu'];
+sort($listAgama);
+
+$listGolongan = ['I', 'II', 'III'];
+
+// membaca file json
+$dataJson = file_get_contents("data/data_karyawan.json");
+// ubah json ke array
+$dataKaryawan = json_decode($dataJson, true);
+
+if (isset($_GET['btnSave'])) {
+    // ambil data setiap input user
+    $nik = $_GET['nik'];
+    $nama = $_GET['nama'];
+    $jenisKelamin = $_GET['jenisKelamin'];
+    $agama = $_GET['agama'];
+    $golonan = $_GET['golongan'];
+    $gajiPokok = $_GET['gajiPokok'];
+
+    // buat array assosiatif baru dan value nya kita ambil dari input user menggunakan method get
+    $dataBaru = [
+        "nik" => $nik,
+        "nama" => $nama,
+        "jenisKelamin" => $jenisKelamin,
+        "agama" => $agama,
+        "golongan" => $golonan,
+        "gajiPokok" => $gajiPokok,
+    ];
+
+    // memasukkan array data baru ke array data karyawan
+    array_push($dataKaryawan, $dataBaru);
+
+    // ubah array ke json
+    $dataToJson = json_encode($dataKaryawan, JSON_PRETTY_PRINT);
+
+    // menulis ke file json
+    file_put_contents("data/data_karyawan.json", $dataToJson);
+}
+
+// baca file json
+$dataJson = file_get_contents("data/data_karyawan.json");
+
+// ubah json ke array
+$dataKaryawan = json_decode($dataJson, true);
+
+function hitungTunjangan($golongan)
+{
+    if ($golongan == "I") {
+        return 1000000;
+    } else if ($golongan == "II") {
+        return 2000000;
+    } else if ($golongan == "III") {
+        return 3000000;
+    } else {
+        return 0;
+    }
+}
+function pajak($gajiPokok, $tunjangan)
+{
+    $pajak = ($gajiPokok + $tunjangan) * 0.05;
+    return $pajak;
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP V5.3 -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="js/bootstrap.min.js"></script>
+    <title>FORM KARYAWAN | VSGA</title>
+</head>
+
+<body>
+
+    <heading>
+        <div class="nav justify-content-center mt-2">
+            <img src="gambar/icons-hero.png" alt="bootstrap" width="100">
+        </div>
+        <div class="nav justify-content-center mt-2">
+            <div class="nav-item">
+                <h2>
+                    FORM KARYAWAN
+                </h2>
+            </div>
+        </div>
+    </heading>
+
+    <!-- Modal Tambah Data Karyawan -->
+    <div class="modal fade" id="add" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Add Data Karyawan</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+
+                    <form action="" method="get" class="needs-validation" novalidate>
+                        <table>
+                            <tr>
+                                <td>
+                                    <div class="form-floating mb-3">
+                                        <input type="number" class="form-control" name="nik" placeholder="NIK" required>
+                                        <div class="valid-feedback">
+                                            Looks Good!
+                                        </div>
+                                        <label for="floatingInput">NIK</label>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <div class="form-floating mb-3">
+                                        <input type="text" class="form-control" name="nama" placeholder="NAMA">
+                                        <div class="valid-feedback">
+                                            Looks Good!
+                                        </div>
+                                        <label for="floatingInput">NAMA</label>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <div class="form-floating mb-3">
+                                        <select class="form-select" name="jenisKelamin" aria-label="Floating label select example">
+                                            <option selected disabled>Pilih</option>
+                                            <option value="1">Laki-Laki</option>
+                                            <option value="2">Perempuan</option>
+                                        </select>
+                                        <div class="valid-feedback">
+                                            Looks Good!
+                                        </div>
+                                        <label for="floatingSelect">Jenis Kelamin</label>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <div class="form-floating mb-3">
+                                        <select class="form-select" name="agama" aria-label="Floating label select example">
+                                            <?php
+
+                                            echo "<option selected disabled>Pilih</option>";
+
+                                            foreach ($listAgama as $agama) {
+                                                echo "<option value='$agama'>$agama</option>";
+                                            }
+
+                                            ?>
+                                        </select>
+                                        <div class="valid-feedback">
+                                            Looks Good!
+                                        </div>
+                                        <label for="floatingSelect">Agama</label>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <div class="form-floating mb-3">
+                                        <select class="form-select" name="golongan" aria-label="Floating label select example">
+                                            <?php
+
+                                            echo "<option selected disabled>Pilih</option>";
+
+                                            foreach ($listGolongan as $golongan) {
+                                                echo "<option value='$golongan'>$golongan</option>";
+                                            }
+
+                                            ?>
+                                        </select>
+                                        <div class="valid-feedback">
+                                            Looks Good!
+                                        </div>
+                                        <label for="floatingSelect">Golongan</label>
+                                    </div>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td>
+                                    <div class="input-group input-group-default mb-3">
+                                        <span class="input-group-text" id="inputGroup-sizing-default">Gaji
+                                            Pokok</span>
+                                        <input type="number" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" name="gajiPokok" required>
+                                        <div class="invalid-feedback">
+                                            You must fill gaji pokok.
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                        </table>
+                        <div class="modal-footer mb-3">
+                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                            <button type="submit" class="btn btn-primary" name="btnSave">Save changes</button>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <main>
+
+        <div class="container-fluid">
+            <div class="row mt-3">
+                <div class="col-md" id="add">
+                    <button class="btn btn-success float-end me-9 mb-5" data-bs-toggle="modal" data-bs-target="#add" name="btnSave">Tambah Data Karyawan</button>
+                </div>
+            </div>
+
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>NIK</th>
+                        <th>NAMA</th>
+                        <th>JENIS KELAMIN</th>
+                        <th>AGAMA</th>
+                        <th>GOLONGAN</th>
+                        <th>GAJI POKOK</th>
+                        <th>TUNJANGAN</th>
+                        <th>PAJAK</th>
+                        <th>GAJI BERSIH</th>
+                        <th>TOKEN</th>
+                    </tr>
+                </thead>
+
+                <tbody>
+                    <?php
+
+                    // melakukan perulangan terhadap data karyawan
+                    foreach ($dataKaryawan as $karyawan) :
+
+                        $gajiPokok = $karyawan['gajiPokok'];
+                        $tunjangan = hitungTunjangan($karyawan['golongan']);
+                        $pajak = pajak($gajiPokok, $tunjangan);
+                        $gajiBersih = ($gajiPokok + $tunjangan) - $pajak;
+
+                        $factory = new Factory;
+                        $generator = $factory->getGenerator(new Strength(Strength::MEDIUM));
+                        $generator = $factory->getMediumStrengthGenerator();
+                        $token = $generator->generateString(10);
+                    ?>
+                        <tr>
+                            <td> <?= $karyawan['nik'] ?> </td>
+                            <td> <?= $karyawan['nama'] ?> </td>
+                            <td> <?= $karyawan['jenisKelamin'] ? "Laki-Laki" : "Perempuan" ?> </td>
+                            <td> <?= $karyawan['agama'] ?> </td>
+                            <td> <?= $karyawan['golongan'] ?> </td>
+                            <td> <?= $karyawan['gajiPokok'] ?> </td>
+                            <td> <?= $tunjangan ?> </td>
+                            <td> <?= $pajak ?> </td>
+                            <td> <?= $gajiBersih ?> </td>
+                            <td> <?= $token ?> </td>
+                        </tr>
+                    <?php endforeach ?>
+                </tbody>
+            </table>
+        </div>
+
+    </main>
+
+</body>
+
+</html>
